5826eb8bcce740d8cda4510bc309adc922413384
mhhan <mun01180@gmail.com>
2024-04-29 17:57:52 +0900

    배포준비 완료 2

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ st.title("ChatPDF")
 st.write("---")
 
 # 파일 업로드
-uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("PDF파일을 올려주세요", type=['pdf'])
 st.write("---")
 
 def pdf_to_document(uploaded_files):
